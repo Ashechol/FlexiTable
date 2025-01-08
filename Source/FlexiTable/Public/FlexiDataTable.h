36631ce8713a9a15fab7d5bcadf4086cef797713
Ashechol <ashechol@qq.com>
2025-01-08 23:36:16 +0800

    Create base class

---
+++ Source/FlexiTable/Public/FlexiDataTable.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "FlexiDataTable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLEXITABLE_API UFlexiDataTable : public UDataTable
+{
+	GENERATED_BODY()
+};
